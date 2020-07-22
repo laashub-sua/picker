617000fcc37fdca292a1460db4e4019fc1c9cc74
tristan <tanshilinmail@gmail.com>
2020-07-22 16:12:30 +0800

    temp

--- try/try4.py
+++ try/try4.py
@@ -1,6 +1,7 @@
-from component import win_program
+from component import derive_win_program
 
 if __name__ == '__main__':
     # target_hwnd = get_target_position_hwnd(1258, 563)
-    title, path = win_program.get_win_exe_path_and_title_by_hwnd(win_program.get_target_position_hwnd(551, 423))
+    title, path = derive_win_program.get_win_exe_path_and_title_by_hwnd(
+        derive_win_program.get_target_position_hwnd(551, 423))
     print(title, path)
